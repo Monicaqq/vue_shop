0f513088a09c675e57abff59fb4b65d85e9c658f
Monica0116 <820128494@qq.com>
2020-03-01 21:59:00 +0800

    完成了登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 导入弹框提示, 挂载到了Vue 原型上, 每个组件都可以用 this 访问$message
+Vue.prototype.$message = Message
