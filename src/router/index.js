6edba73dd8dc1fb6cd274e5a7b51cfa71b4f488a
Monica0116 <820128494@qq.com>
2020-03-02 09:29:39 +0800

    '添加备注'

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,7 @@ router.beforeEach((to, from, next) => {
   // to 将要访问的路径, from :从哪个路径跳转而来
   // next: 是一个函数,表示放行
   // next() 放行 或 next('/login') 强制跳转
+  // 用户访问的是登录页, 直接放行
   if (to.path === '/login') return next()
   // 先获取token
   const tokenStr = window.sessionStorage.getItem('token')
